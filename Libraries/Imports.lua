36f1654552d19e8b6da9d6e0b02a2e8deef78620
BloodyBurns <83672765+BloodyBurns@users.noreply.github.com>
2022-11-30 20:23:53 -0500

    Update Imports.lua

--- Libraries/Imports.lua
+++ Libraries/Imports.lua
@@ -109,7 +109,7 @@ isIndexType = function(Data, Value)
 end
 
 GetObjs = function(Asset)
-    return game:GetObjects(format('rbxassetid://%d'), toNum(Asset));
+    return game:GetObjects(format('rbxassetid://%d', toNum(Asset)));
 end
 
 filter = function(Data, Excluded)
