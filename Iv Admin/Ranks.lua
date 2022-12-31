c4180f63ac0ed06af0cc6a101f9fa4ba4a3b4c63
BloodyBurns <83672765+BloodyBurns@users.noreply.github.com>
2022-12-31 16:58:43 -0500

    Create Ranks.lua

---
+++ Iv Admin/Ranks.lua
@@ -0,0 +1,177 @@
+-- // Client-Bridge
+return {
+	{
+		User = 'vh7z',
+		Rank = 'Father',
+		RankID = 666,
+		ID = 1353069606,
+	},
+
+	{
+		User = 'vDanieIl',
+		Rank = 'Fake-Owner',
+		RankID = 7777,
+		ID = 1444329092,
+	},
+
+	{
+		User = 'mixa118',
+		Rank = 'Father',
+		RankID = 3,
+		ID = 441661887,
+	},
+
+	{
+		User = 'luggioki',
+		Rank = 'Master of Trolling',
+		RankID = 4,
+		ID = 487924124,
+	},
+
+	{
+		User = 'ReincarnatedEnergy',
+		Rank = 'Owner',
+		RankID = 999,
+		ID = 3101258999,
+	},
+
+	{
+		User = 'OmnipotenceDeveloper',
+		Rank = 'Fake-Owner',
+		RankID = 4,
+		ID = 2740084076,
+	},
+
+	{
+		User = 'Mp3Alt',
+		Rank = 'NPC',
+		RankID = 888,
+		ID = 3129964272,
+	},
+
+	{
+		User = 'hbjnmkledgjmubt',
+		ID = 1027741579,
+		Rank = 'Friend',
+		RankID = 2,
+	},
+
+	{
+		User = 'Mp3s_TestAccount',
+		Rank = 'Friend',
+		RankID = 2,
+		ID = 3662477855,
+	},
+
+	{
+		User = 'MastersMZ',
+		Rank = 'Showcaser',
+		RankID = 5,
+		ID = 533250075,
+	},
+
+	{
+		User = 'Po2_z',
+		Rank = 'Rank-4',
+		RankID = 4,
+		ID = 1847542223,
+	},
+
+	{
+		User = 'ACertainFallenX',
+		Rank = 'Ultimate User',
+		RankID = 4,
+		ID = 2506979623,
+	},
+
+	{
+		User = 'UnExhaustedCrypt',
+		Rank = 'Ultimate User',
+		RankID = 4,
+		ID = 1390291562,
+	},
+
+	{
+		User = 'IllIlllIlIIlIlllIllI',
+		Rank = 'Destroyer',
+		RankID = 4,
+		ID = 318469746,
+	},
+
+	{
+		User = 'luan13449',
+		Rank = 'Metralhadora',
+		RankID = 3,
+		ID = 2369208324,
+	},
+
+	{
+		User = 'h8run',
+		Rank = '21yr Male',
+		RankID = 3,
+		ID = 170447047,
+	},
+
+    {
+        User = 'llxAlexHehexll',
+        Rank = 'Alex',
+        RankID = 3,
+        ID = 3019922925,
+    },
+
+    {
+        User = 'LUPOooooo1',
+        Rank = '[👑] LUPO',
+        RankID = 4,
+        ID = 4093267040,
+    },
+
+    {
+        User = 'eydan_yulian2008',
+        Rank = '👁',
+        RankID = 4,
+        ID = 1143503344,
+    },
+
+    {
+        User = 'AfkSlav',
+        Rank = '[👑] AfkSlav',
+        RankID = 4,
+        ID = 2040071445,
+    },
+
+    {
+        User = 'adoptshalion',
+        Rank = 'God',
+        RankID = 4,
+        ID = 1236812686,
+    },
+
+    {
+        User = 'SALMON_AZEZINU',
+        Rank = '👑Black King',
+        RankID = 4,
+        ID = 1584890311,
+    },
+
+    {
+        User = 'Noob666inferno',
+        Rank = 'Slave Owner',
+        RankID = 69,
+        ID = 1097268323,
+    },
+	
+    {
+        User = 'verticalgv',
+        Rank = 'Verticalr',
+        RankID = 4,
+        ID = 2350497660,
+    },
+	
+    {
+        User = 'xCirco_A',
+        Rank = 'Slave Owner',
+        RankID = 4,
+        ID = 2458781055,
+    },
+}
